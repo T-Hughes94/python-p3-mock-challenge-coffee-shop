636e24635dffec81b74f82fac73f19d7dc2ef7f6
Terence Hughes <Terencehughes450@gmail.com>
2023-09-19 12:49:54 -0600

    finished lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,6 +2,17 @@ class Coffee:
     def __init__(self, name):
         self.name = name
         
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 3 <= len(name) and not hasattr(self, 'name'):
+            self._name = name
+        else:
+            print('This is not a valid name.')
+    
+    name = property(get_name, set_name)
+   
     def orders(self):
         pass
     
@@ -17,7 +28,18 @@ class Coffee:
 class Customer:
     def __init__(self, name):
         self.name = name
-        
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name,str) and 1 <= len(name) and len(name)<= 15:
+            self._name = name   
+        else:
+            print('This is not a valid name')
+
+    name = property(get_name, set_name)
+    
     def orders(self):
         pass
     
@@ -32,3 +54,17 @@ class Order:
         self.customer = customer
         self.coffee = coffee
         self.price = price
+    
+    def get_price(self):
+        return self._price
+    
+    def set_price(self, price):
+        if isinstance(price, float) and 1.0 <= price and price <= 10.0 and not hasattr(self,'price'):
+            self._price = price
+        else:
+            print('this is not a valid input.')
+
+    price = property(get_price, set_price)
+
+    
+
